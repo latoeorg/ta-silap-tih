b09cf609749489247aa552fab7bc3cb7f00874b0
lazlanrafar <lazlanrafar@gmail.com>
2025-06-05 10:41:13 +0800

    feat(app): slicing sidebar and header

--- web/components/layout/AppSidebar.vue
+++ web/components/layout/AppSidebar.vue
@@ -1,64 +1,80 @@
 <script setup lang="ts">
-import type { NavGroup, NavLink, NavSectionTitle } from '~/types/nav'
-import { navMenu, navMenuBottom } from '~/constants/menus'
+import type { NavGroup, NavLink, NavSectionTitle } from "~/types/nav";
+import { navMenu, navMenuBottom } from "~/constants/menus";
 
-function resolveNavItemComponent(item: NavLink | NavGroup | NavSectionTitle): any {
-  if ('children' in item)
-    return resolveComponent('LayoutSidebarNavGroup')
+function resolveNavItemComponent(
+  item: NavLink | NavGroup | NavSectionTitle
+): any {
+  if ("children" in item) return resolveComponent("LayoutSidebarNavGroup");
 
-  return resolveComponent('LayoutSidebarNavLink')
+  return resolveComponent("LayoutSidebarNavLink");
 }
 
 const teams: {
-  name: string
-  logo: string
-  plan: string
+  name: string;
+  logo: string;
+  plan: string;
 }[] = [
   {
-    name: 'Acme Inc',
-    logo: 'i-lucide-gallery-vertical-end',
-    plan: 'Enterprise',
+    name: "Acme Inc",
+    logo: "i-lucide-gallery-vertical-end",
+    plan: "Enterprise",
   },
   {
-    name: 'Acme Corp.',
-    logo: 'i-lucide-audio-waveform',
-    plan: 'Startup',
+    name: "Acme Corp.",
+    logo: "i-lucide-audio-waveform",
+    plan: "Startup",
   },
   {
-    name: 'Evil Corp.',
-    logo: 'i-lucide-command',
-    plan: 'Free',
+    name: "Evil Corp.",
+    logo: "i-lucide-command",
+    plan: "Free",
   },
-]
+];
 
 const user: {
-  name: string
-  email: string
-  avatar: string
+  name: string;
+  email: string;
+  avatar: string;
 } = {
-  name: 'Dian Pratama',
-  email: 'dianpratama2@gmail.com',
-  avatar: '/avatars/avatartion.png',
-}
+  name: "Dian Pratama",
+  email: "dianpratama2@gmail.com",
+  avatar: "/avatars/avatartion.png",
+};
 
-const { sidebar } = useAppSettings()
+const { sidebar } = useAppSettings();
 </script>
 
 <template>
-  <Sidebar :collapsible="sidebar.collapsible" :side="sidebar.side" :variant="sidebar.variant">
+  <Sidebar
+    :collapsible="sidebar.collapsible"
+    :side="sidebar.side"
+    :variant="sidebar.variant"
+  >
     <SidebarHeader>
       <LayoutSidebarNavHeader :teams="teams" />
-      <Search />
+      <!-- <Search /> -->
     </SidebarHeader>
     <SidebarContent>
       <SidebarGroup v-for="(nav, indexGroup) in navMenu" :key="indexGroup">
         <SidebarGroupLabel v-if="nav.heading">
           {{ nav.heading }}
         </SidebarGroupLabel>
-        <component :is="resolveNavItemComponent(item)" v-for="(item, index) in nav.items" :key="index" :item="item" />
+        <component
+          :is="resolveNavItemComponent(item)"
+          v-for="(item, index) in nav.items"
+          :key="index"
+          :item="item"
+        />
       </SidebarGroup>
       <SidebarGroup class="mt-auto">
-        <component :is="resolveNavItemComponent(item)" v-for="(item, index) in navMenuBottom" :key="index" :item="item" size="sm" />
+        <component
+          :is="resolveNavItemComponent(item)"
+          v-for="(item, index) in navMenuBottom"
+          :key="index"
+          :item="item"
+          size="sm"
+        />
       </SidebarGroup>
     </SidebarContent>
     <SidebarFooter>
@@ -68,6 +84,4 @@ const { sidebar } = useAppSettings()
   </Sidebar>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- web/components/layout/SidebarNavFooter.vue
+++ web/components/layout/SidebarNavFooter.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
-import { useSidebar } from '~/components/ui/sidebar'
+import { useSidebar } from "~/components/ui/sidebar";
 
-defineProps<{
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}>()
+const { isMobile, setOpenMobile } = useSidebar();
 
-const { isMobile, setOpenMobile } = useSidebar()
+const authStore = useAuthStore();
+const user = computed(() => authStore.user);
 
 function handleLogout() {
-  navigateTo('/login')
+  authStore.logout();
+  navigateTo("/login");
 }
 
-const showModalTheme = ref(false)
+const showModalTheme = ref(false);
 </script>
 
 <template>
@@ -28,14 +24,19 @@ const showModalTheme = ref(false)
             class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
           >
             <Avatar class="h-8 w-8 rounded-lg">
-              <AvatarImage :src="user.avatar" :alt="user.name" />
+              <AvatarImage src="/avatars/avatartion.png" :alt="user?.name" />
               <AvatarFallback class="rounded-lg">
-                {{ user.name.split(' ').map((n) => n[0]).join('') }}
+                {{
+                  user?.name
+                    .split(" ")
+                    .map((n) => n[0])
+                    .join("")
+                }}
               </AvatarFallback>
             </Avatar>
             <div class="grid flex-1 text-left text-sm leading-tight">
-              <span class="truncate font-semibold">{{ user.name }}</span>
-              <span class="truncate text-xs">{{ user.email }}</span>
+              <span class="truncate font-semibold">{{ user?.name }}</span>
+              <span class="truncate text-xs">{{ user?.email }}</span>
             </div>
             <Icon name="i-lucide-chevrons-up-down" class="ml-auto size-4" />
           </SidebarMenuButton>
@@ -48,27 +49,25 @@ const showModalTheme = ref(false)
           <DropdownMenuLabel class="p-0 font-normal">
             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
               <Avatar class="h-8 w-8 rounded-lg">
-                <AvatarImage :src="user.avatar" :alt="user.name" />
+                <AvatarImage src="/avatars/avatartion.png" :alt="user?.name" />
                 <AvatarFallback class="rounded-lg">
-                  {{ user.name.split(' ').map((n) => n[0]).join('') }}
+                  {{
+                    user?.name
+                      .split(" ")
+                      .map((n) => n[0])
+                      .join("")
+                  }}
                 </AvatarFallback>
               </Avatar>
               <div class="grid flex-1 text-left text-sm leading-tight">
-                <span class="truncate font-semibold">{{ user.name }}</span>
-                <span class="truncate text-xs">{{ user.email }}</span>
+                <span class="truncate font-semibold">{{ user?.name }}</span>
+                <span class="truncate text-xs">{{ user?.email }}</span>
               </div>
             </div>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <Icon name="i-lucide-sparkles" />
-              Upgrade to Pro
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
+            <!-- <DropdownMenuItem>
               <Icon name="i-lucide-badge-check" />
               Account
             </DropdownMenuItem>
@@ -81,14 +80,8 @@ const showModalTheme = ref(false)
             <DropdownMenuItem>
               <Icon name="i-lucide-bell" />
               Notifications
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem as-child>
-              <NuxtLink to="https://github.com/dianprata/nuxt-shadcn-dashboard" external target="_blank">
-                <Icon name="i-lucide-github" />
-                Github Repository
-              </NuxtLink>
-            </DropdownMenuItem>
+            </DropdownMenuItem> -->
+            <!-- <DropdownMenuSeparator /> -->
             <DropdownMenuItem @click="showModalTheme = true">
               <Icon name="i-lucide-paintbrush" />
               Theme
@@ -117,6 +110,4 @@ const showModalTheme = ref(false)
   </Dialog>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
